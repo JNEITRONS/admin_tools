f7a86a95d300221a58b60e4151ddb728237c8354
JNEITRONS <33489595+JNEITRONS@users.noreply.github.com>
2018-09-28 10:03:29 +0300

    Update init.lua

--- init.lua
+++ init.lua
@@ -1,1 +1,7 @@
+admin_tools = {}
+admin_tools.version = "0.1"
+admin_tools.path = minetest.get_modpath("admin_tools")
 
+dofile(admin_tool.path .. "/tools.lua")
+
+printf("[Mod] Admin tools ["..admin_tools.version.."] loaded")
